254d1ad773e80c61e44d0a77e56bbcda12b9a280
3ele-projects <github@3ele.de>
2020-03-09 13:51:40 +0100

    add remove_umlaut function

--- main.py
+++ main.py
@@ -45,18 +45,41 @@ def create_tmp_files(mailBody):
         tmp.close()
 
 
+def remove_umlaut(string):
+    """
+    Removes umlauts from strings and replaces them with the letter+e convention
+    :param string: string to remove umlauts from
+    :return: unumlauted string
+    """
+    u = 'ü'.encode()
+    U = 'Ü'.encode()
+    a = 'ä'.encode()
+    A = 'Ä'.encode()
+    o = 'ö'.encode()
+    O = 'Ö'.encode()
+    ss = 'ß'.encode()
 
+    string = string.encode()
+    string = string.replace(u, b'ue')
+    string = string.replace(U, b'Ue')
+    string = string.replace(a, b'ae')
+    string = string.replace(A, b'Ae')
+    string = string.replace(o, b'oe')
+    string = string.replace(O, b'Oe')
+    string = string.replace(ss, b'ss')
+
+    string = string.decode('utf-8')
+    return string
          
 
 def main():
 
     with MailBox(IMAP_SERVER).login(EMAIL_ACCOUNT, EMAIL_PASSWORD, initial_folder='INBOX') as mailbox:
         for msg in mailbox.fetch(Q('UNSEEN')):
-       # for msg in mailbox.fetch():
+        for msg in mailbox.fetch():
 
-            mailBody = msg.text
-            print(isascii(mailBody))
-            create_tmp_files(mailBody)
+        mailBody = msg.text
+        create_tmp_files(remove_umlaut(mailBody))
 
 
 
